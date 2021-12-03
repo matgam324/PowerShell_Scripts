92725eaa679a661e53d1de114324c10cddb8648d
Matt Gammon <matt@gammon.org.uk>
2021-12-03 16:24:35 +0000

    Add files via upload
    Script to create snapshots of VM based on the Tag link to tehm

---
+++ PowerCLI/CreateSnapshotVMsBaseOnTag.ps1
@@ -0,0 +1,31 @@
+[CmdletBinding()]  
+  
+Param(  
+  
+	[Parameter(Mandatory=$true,Position=1)]  
+	# Asking for vCenter name
+	[string]$VCenterServer,  
+	 
+	[Parameter(Mandatory=$True,Position=2)]  
+	# Asking for the name of the Cluster
+	[string]$ClusterName,
+
+	[Parameter(Mandatory=$True,Position=3)]  
+	# Asking for the name of the tag
+	[string]$TagName
+  
+)
+# connecting to vCenter server
+Connect-VIServer $VCenterServer
+# creating the list of VMs base on Cluster name and Tag name
+$VMs = @(Get-Cluster $ClusterName| Get-VM -Tag $TagName)
+
+# create Snapshot for each vm listed in $VMs
+Foreach ($VM in $VMs) {
+
+# take snapshot and give perset name and time and data when it was taken
+Get-VM $VM | new-Snapshot -Name PrePatching -Description "Created $(Get-Date)" -RunAsync -Confirm:$false
+
+}
+# disconnect from vCenter server
+Disconnect-VIServer * -Force -Confirm:$false
